8d588322cda1cc8485801850c189315042c709e8
vijai <vijai.djearam@gmail.com>
2024-03-05 12:30:59 +0100

    Update clonezilla-recovery-partition.ps1

--- powershell/clonezilla-recovery-partition.ps1
+++ powershell/clonezilla-recovery-partition.ps1
@@ -99,9 +99,9 @@ Start-BitsTransfer -Source https://raw.githubusercontent.com/vijaidjearam/clonez
 diskpart /s $env:temp\clonezilla-part-to-recovery.txt
 Remove-Item $env:temp\clonezilla-part-to-recovery.txt -Force
 
-# downloading the diskpart script for making the backup partition -> recovery partition
+## downloading the diskpart script for making the backup partition -> recovery partition
 Start-BitsTransfer -source https://raw.githubusercontent.com/vijaidjearam/clonezilla_recovery/main/powershell/backup-part-to-recovery.txt -Destination $env:temp\backup-part-to-recovery.txt
-# downloading the batchscript to make the backup partition -> recovery partition
+## downloading the batchscript to make the backup partition -> recovery partition
 Start-BitsTransfer -Source https://raw.githubusercontent.com/vijaidjearam/clonezilla_recovery/main/powershell/backup-part-to-recovery.bat -Destination $env:temp\backup-part-to-recovery.bat
 write-host "Take a backup of the system by rebooting the system and booting to clonezilla, after the backup is completed run the backup-part-to-recovery.bat to seal the backup recovery partition" -ForegroundColor Green
 
